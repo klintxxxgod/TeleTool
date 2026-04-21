a8582c24148316430f8ddb2acd696a9c89de2487
klintxxxgod <owner.klint@gmail.com>
2026-04-21 19:46:58 +0300

    111

--- menu/__init__.py
+++ menu/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from .banner import banner, print_separator, print_menu_item, print_header
 from .session_analyzer import session_analyzer_menu
 from .session_creator_qr import session_creator_qr_menu
@@ -7,13 +6,3 @@ from .session_account_info import session_account_info_menu
 from .session_to_tdata import session_to_tdata_menu
 from .tdata_to_session import tdata_to_session_menu
 from .cleaner_and_info import cleaner_menu
-=======
-from .banner import banner, print_separator, print_menu_item, print_header
-from .session_analyzer import session_analyzer_menu
-from .session_creator_qr import session_creator_qr_menu
-from .session_creator_code import session_creator_code_menu
-from .session_account_info import session_account_info_menu
-from .session_to_tdata import session_to_tdata_menu
-from .tdata_to_session import tdata_to_session_menu
-from .cleaner_and_info import cleaner_menu
->>>>>>> 52bae208c01c84fa139228c18362d9264015306d
